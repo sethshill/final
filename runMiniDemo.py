47f435b9bf6e361fcf07857fb1782399961a83dc
Seth's Pi <sss85@zips.uakron.edu>
2017-04-11 17:35:02 -0400

    runMiniDemo now has Visualize class, but doesn't show separate shapes)

--- runMiniDemo.py
+++ runMiniDemo.py
@@ -1,15 +1,11 @@
 # Test functions all together
-# Import input generation functions
-from bibliopixel.shapeGen import*
-import random
-from bibliopixel.interpretMood import*
-from time import sleep
 
+#### Setup #####
 # Setup configurations
 from bibliopixel.led import*
 from bibliopixel.drivers.APA102 import*
+from bibliopixel.animation import*
 
-#### Setup #####
 # Global Vars
 NUM = 8*8
 
@@ -17,12 +13,30 @@ NUM = 8*8
 driver = DriverAPA102(NUM, c_order = ChannelOrder.BGR) # 64 LEDs, 2 MHz speed using SPI, BRG order
 led = LEDMatrix(driver,rotation = 2,vert_flip = True) # Correct Orientation
 
+##### Key Functions #####
+# Import input generation functions
+from bibliopixel.genShapes import genShapes
+from bibliopixel.interpretMood import*
+from time import sleep
+
+# For testing
+import random
+
 class Visualize(BaseMatrixAnim):
-	def __init__(self, led):
-		super(MatrixTest, self).__init__(led)
+	def __init__(self, led, color, shapes):
+		super(Visualize, self).__init__(led)
+		self.colorVal = color
+		self.shapeVals = shapes
 		
 	def step(self, amt = 1):
+		for i in range(len(self.shapeVals)):
+			self._led.fillRect(self.shapeVals[i][0], self.shapeVals[i][1], self.shapeVals[i][2], self.shapeVals[i][3], self.colorVal)
 		self._step += amt
+	
+	def testRect(self, x, y, w, h, color = None):
+		self._led.fillRect(x, y, w, h, color)	
+		print 'inside function'
+		print x
 		
 def done():
 	print 'Done'
@@ -32,6 +46,11 @@ def done():
 sampleVolumes = [0]*10
 for i in range(0, 10):
 	sampleVolumes[i] = random.random()
-# make functions
+# Get color and shape from values
 color = genColor(genMood(key='Cma'))
-makeDisplay(sampleVolumes, 100, color)
+shapes = genShapes(sampleVolumes)
+# Create object and set values
+visualize = Visualize(led, color, shapes)
+# Run object
+#~ visualize.run(sleep = 500)
+visualize.testRect(2,2,3,3,(255,0,0))
